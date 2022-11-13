53eb7153f2dc0f5643c34d6b09be9a3224eb597a
Faradifa <mghfrhrntyfrdf@gmail.com>
2022-11-13 15:39:25 +0700

    CardCarousel done

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,14 +1,18 @@
 import {StyleSheet, Text, TouchableOpacity, Image, View} from 'react-native';
 import React from 'react';
-import Component from './../../components';
+import Component, { BottomNav } from './../../components';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const index = () => {
   return (
     <View style={{flexDirection: 'column'}}>
+      <ScrollView>
       <Component.HomeComp.Banner />
       <Component.HomeComp.SaldoKecil />
       <Component.HomeComp.Fitur />
       <Component.HomeComp.CardCarousel />
+      </ScrollView>
+      <BottomNav/>
     </View>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,9 @@
 import BottomNav from './BottomNav';
-import Banner from './Banner';
-import SaldoKecil from './SaldoKecil';
 import CustomButton from './CustomButton';
 import CustomInput from './CustomInput';
 import HomeComp from './HomeComp';
 
 export {BottomNav};
-export {Banner};
-export {SaldoKecil};
 export {CustomButton};
 export {CustomInput};
 export {HomeComp};
